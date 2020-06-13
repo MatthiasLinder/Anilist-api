3ba5e9f0a45f5592adcff05403e9aa17e730c2f1
MatthiasLinder <matthias.linder@tptlive.ee>
2020-06-13 13:19:36 +0300

    Added image upload.
    Used Multer for managing and parsing the upload.

--- api/models/anime.js
+++ api/models/anime.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 const animeSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: { type: String, required: true},
-    description: { type: String, required: true }
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    episode_count: {type: Number, required: true},
+    animeImage: {type: String, required: true}
 })
 
 module.exports = mongoose.model('Anime', animeSchema);
--- api/routes/anime.js
+++ api/routes/anime.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true)
+    } else{
+        cb(null, false)
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+      fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+})
 
 const Anime = require('../models/anime')
 
 router.get('/', (req, res, next) => {
    Anime.find()
-       .select('name description _id')
+       .select('name description category episode_count animeImage _id')
        .exec()
        .then(docs => {
            const response = {
@@ -15,6 +41,9 @@ router.get('/', (req, res, next) => {
                    return {
                        name: doc.name,
                        description: doc.description,
+                       category: doc.category,
+                       episode_count: doc.episode_count,
+                       animeImage: doc.animeImage,
                        _id: doc._id,
                        request: {
                            type: 'GET',
@@ -34,12 +63,15 @@ router.get('/', (req, res, next) => {
        })
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('animeImage'), (req, res, next) => {
     const anime = new Anime({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         description: req.body.description,
-    });
+        category: req.body.category,
+        episode_count: req.body.episode_count,
+        animeImage: req.file.path
+    })
     anime
         .save()
         .then(result => {
@@ -49,10 +81,12 @@ router.post('/', (req, res, next) => {
                 createdAnime: {
                     name: result.name,
                     description: result.description,
+                    category: result.category,
+                    episode_count: result.episode_count,
                     _id: result._id,
                     request: {
                         type: 'GET',
-                        url: "http://localhost:3000/products/" + result._id
+                        url: "http://localhost:3000/anime/" + result._id
                     }
                 }
             })
@@ -68,7 +102,7 @@ router.post('/', (req, res, next) => {
 router.get('/:animeId', (req, res, next) => {
     const id = req.params.animeId
     Anime.findById(id)
-        .select('name description _id')
+        .select('name description category episode_count animeImage _id')
         .exec()
         .then(doc => {
             console.log("From database:", doc)
@@ -90,6 +124,40 @@ router.get('/:animeId', (req, res, next) => {
             res.status(500).json({error: err})
         })
 })
+
+/*
+router.get('/episodes', (req, res, next) => {
+    Anime.find({ episode_count:  {$gte : 12}})
+        .select('name description category episode_count _id')
+        .exec()
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                anime: docs.map(doc => {
+                    return {
+                        name: doc.name,
+                        description: doc.description,
+                        category: doc.category,
+                        episode_count: doc.episode_count,
+                        _id: doc._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/anime/' + doc._id
+                        }
+                    }
+                })
+            }
+            console.log(docs)
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            })
+        })
+})
+ */
 
 router.patch('/:animeId', (req, res, next) => {
     const id = req.params.animeId;
